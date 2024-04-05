7bca0357cf3403ca105a5c59580f119ba3f0e12a
Assyattt <assyat.t.p@gmail.com>
2024-04-05 16:47:47 +0500

    Version alpha 0.1 Problem 8 The very first version of the program.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         if (s.charAt(0) < 48 && s.charAt(0) > 71){
             return false;
         }
-        return IsConsistOfDigits(s.substring(1,s.length()));
+        return IsConsistOfDigits(s.substring(1));
     }
     public static void main(String[] args) {
         System.out.println(IsConsistOfDigits("123A56"));
