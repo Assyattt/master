e76ecb5dfb08b0c23325fc0a723f9693768f2330
Assyattt <assyat.t.p@gmail.com>
2024-04-07 21:40:26 +0500

    Version alpha 0.3 Problem 1 is added.

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,22 @@ public class Main {
         }
         return IsConsistOfDigits(s.substring(1));
     }
+
+    public static int MinInArray(int length_of_array, int[] array){
+        if (array == null) {
+            throw new IllegalArgumentException("Array is empty or it has other length");
+        }
+        if (length_of_array == 1) {
+            return array[length_of_array-1];
+        }
+        if (array[length_of_array-1]<array[length_of_array-2]){
+            array[length_of_array-2] = array[length_of_array-1];
+        }
+        return MinInArray(length_of_array-1, array);
+    }
+
     public static void main(String[] args) {
-        System.out.println(IsConsistOfDigits("123A56"));
+        int[] numbers = {0, 5, 32, 3, 45};
+        System.out.println(MinInArray(5, numbers));
     }
 }
